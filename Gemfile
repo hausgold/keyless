68104858ae6a372ff7a2fa9d9c333c99998a836e
Hermann Mayer <hermann.mayer92@gmail.com>
2025-06-28 13:09:07 +0200

    Drop Ruby 2 and end of life Rails. (#13)
    Signed-off-by: Hermann Mayer <hermann.mayer92@gmail.com>

--- Gemfile
+++ Gemfile
@@ -9,10 +9,10 @@ gemspec
 
 # Development dependencies
 gem 'appraisal', '~> 2.4'
-gem 'bundler', '~> 2.3'
+gem 'bundler', '~> 2.6'
 gem 'countless', '~> 1.1'
 gem 'guard-rspec', '~> 4.7'
-gem 'railties', '>= 6.1'
+gem 'railties', '>= 7.1'
 gem 'rake', '~> 13.0'
 gem 'rspec', '~> 3.12'
 gem 'rubocop'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ Time.zone = 'Europe/Berlin'
 # rubocop:enable Rails/TimeZoneAssignment
 
 # Load all support helpers and shared examples
-Dir[File.join(__dir__, 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[File.join(__dir__, 'support', '**', '*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
